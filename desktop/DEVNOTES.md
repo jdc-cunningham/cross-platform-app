20f8343b1fbba3b79c86b1f2adb875a7f5150752
Jacob Cunningham <jcun4128@gmail.com>
2020-03-31 20:54:42 -0500

    added inner window, more menu functionality

---
+++ desktop/DEVNOTES.md
@@ -0,0 +1,4 @@
+Look into
+
+- how to add SSL, saw in console
+- make a base install regarding the platform detection, re-usable methods like the window generator
--- desktop/main.js
+++ desktop/main.js
@@ -3,8 +3,10 @@ const url = require('url');
 const path = require('path');
 
 const { app, BrowserWindow, Menu } = electron;
+const macPlatform = process.platform == 'darwin';
 
 let mainWindow;
+let addWindow;
 
 // listen for app to be ready
 app.on('ready', () => {
@@ -18,12 +20,39 @@ app.on('ready', () => {
         slashes: true
     }));
 
+    // quit app when closed
+    mainWindow.on('closed', () => {
+        app.quit();
+    });
+
     // build menu from template
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
 
     // insert menu
     Menu.setApplicationMenu(mainMenu);
 });
+
+// handle create add window
+const createAddWindow = () => {
+    // create new window
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add todo item'
+    });
+
+    // load html file into window
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // reduce memory usage
+    addWindow.on('close', () => {
+        addWindow = null;
+    });
+};
 
 // create menu template
 const mainMenuTemplate = [
@@ -32,13 +61,16 @@ const mainMenuTemplate = [
         submenu: [
             {
                 label: 'Add Todo',
+                click() {
+                    createAddWindow();
+                }
             },
             {
                 label: 'Clear todos',
             },
             {
                 label: 'Quit',
-                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                accelerator: macPlatform ? 'Command+Q' : 'Ctrl+Q',
                 click() {
                     app.quit();
                 }
@@ -46,3 +78,27 @@ const mainMenuTemplate = [
         ]
     }
 ];
+
+// if Mac add empty object to menu
+if (macPlatform) {
+    mainMenuTemplate.unshift({});
+}
+
+// add dev tools menu itme if not in prod
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label:'Dev tools',
+        submenu: [
+            {
+                label: 'Toggle DevTools',
+                accelerator: macPlatform ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow) { // want devtools to show up on active window
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    });
+}
