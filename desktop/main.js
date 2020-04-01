c7ee9ab8113ee13d4b77bbce4df6507300d50ae4
Jacob Cunningham <jcun4128@gmail.com>
2020-03-31 20:22:39 -0500

    basic menu, quittable by menu and shortcut

--- desktop/main.js
+++ desktop/main.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
@@ -10,10 +10,39 @@ let mainWindow;
 app.on('ready', () => {
     // create new window
     mainWindow = new BrowserWindow({});
-    // load html file ijnto window
+
+    // load html file into window
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'mainWindow.html'),
         protocol: 'file:',
         slashes: true
     }));
+
+    // build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+    // insert menu
+    Menu.setApplicationMenu(mainMenu);
 });
+
+// create menu template
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Add Todo',
+            },
+            {
+                label: 'Clear todos',
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
