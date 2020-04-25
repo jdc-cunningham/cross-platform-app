2b752333ef0bb70c5c01ebb66201ac9ba7a856aa
Jacob Cunningham <jcun4128@gmail.com>
2020-04-25 13:10:17 -0500

    progress save, working on note taking module

--- desktop/reactjs/src/App.js
+++ desktop/reactjs/src/App.js
@@ -5,7 +5,7 @@ import Dock from './components/dock/Dock';
 import AppWindow from './components/app-window/AppWindow';
 
 const App = () => {
-	const [activeApp, setActiveApp] = useState(null);
+	const [activeApp, setActiveApp] = useState("notes");
 
 	return (
 		<div className="App">
--- desktop/reactjs/src/modules/note-taking-app/NoteTakingApp.js
+++ desktop/reactjs/src/modules/note-taking-app/NoteTakingApp.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './NoteTakingApp.scss';
 
 const NoteTakingApp = (props) => {
+    const noteNameInput = useRef(null);
+    const noteCreateBtn = useRef(null);
+    const noteBodyInput = useRef(null);
+
     return (
         <div className="cpa__module" id="module--note-taking-app">
-            <h2>Note taking app</h2>
+            <div className="module-notes__header">
+                <input type="text" placeholder="search note" className="module-notes__search-bar"/>
+            </div>
+            <div className="module-notes__body">
+                <textarea className="modules-notes__text-area" placeholder="write..."></textarea>
+            </div>
         </div>
     )
 }
