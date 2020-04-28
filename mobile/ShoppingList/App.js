b307bf5b2092a8534f1678b26c1a5c377c132f99
Jacob Cunningham <jcun4128@gmail.com>
2020-04-28 01:27:37 -0500

    working demo code

--- mobile/ShoppingList/App.js
+++ mobile/ShoppingList/App.js
@@ -1,114 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { useState } from 'react';
+import { View, StyleSheet, FlatList, Alert } from 'react-native';
+import Header from './components/Header.js';
+import ListItem from './components/ListItem.js';
+import AddItem from './components/AddItem.js';
+import uuid from 'uuid-random';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const App = () => {
+    const [items, setItems] = useState([
+        {id: uuid(), text: 'Milk'},
+        {id: uuid(), text: 'Eggs'},
+        {id: uuid(), text: 'Break'},
+        {id: uuid(), text: 'Chips'},
+    ]);
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+    const deleteItem = ( itemId ) => {
+        console.log('delete', itemId);
+        setItems(prevItems => {
+            return prevItems.filter(item => item.id !== itemId);
+        });
+    }
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+    const addItem = ( itemTitle ) => {
+        if (!itemTitle) {
+            Alert.alert('Error', 'Please enter an item', [{text: 'Ok'}], {cancelable: true});
+            return;
+        }
+
+        setItems(prevItems => {
+            prevItems.push({
+                id: uuid(),
+                text: itemTitle
+            });
+            return prevItems;
+        })
+    }
+
+    return (
+        <View style={styles.container}>
+            <Header title="Shopping List" />
+            <AddItem addItem={ addItem } />
+            <FlatList
+                data={ items }
+                renderItem={ ({item}) => (
+                    <ListItem item={ item } deleteItem={ deleteItem } />
+                )}/>
+        </View>
+    )
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+    container: {
+        flex: 1
+    }
 });
 
 export default App;
