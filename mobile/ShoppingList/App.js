e4c4e8caf888bb3c726cb58f3953c9ad3020f7d8
Jacob Cunningham <jcun4128@gmail.com>
2020-04-28 20:49:20 -0500

    working menu with module choice

--- mobile/ShoppingList/App.js
+++ mobile/ShoppingList/App.js
@@ -1,57 +1,39 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, FlatList, Alert } from 'react-native';
 import Header from './components/Header.js';
-import ListItem from './components/ListItem.js';
-import AddItem from './components/AddItem.js';
-import uuid from 'uuid-random';
 
 const App = () => {
-    const [items, setItems] = useState([
-        {id: uuid(), text: 'Milk'},
-        {id: uuid(), text: 'Eggs'},
-        {id: uuid(), text: 'Break'},
-        {id: uuid(), text: 'Chips'},
-    ]);
+    const subModules = [
+        {id: "1",
+        name: "notes"},
+        {id: "2",
+        name: "draw"}
+    ];
 
-    const deleteItem = ( itemId ) => {
-        console.log('delete', itemId);
-        setItems(prevItems => {
-            return prevItems.filter(item => item.id !== itemId);
-        });
+    const [activeModule, setActiveModule] = useState(""); // still using strings, should enumerate
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
     }
 
-    const addItem = ( itemTitle ) => {
-        if (!itemTitle) {
-            Alert.alert('Error', 'Please enter an item', [{text: 'Ok'}], {cancelable: true});
-            return;
-        }
-
-        setItems(prevItems => {
-            prevItems.push({
-                id: uuid(),
-                text: itemTitle
-            });
-            return prevItems;
-        })
+    const selectModule = ( selectedModule ) => {
+        setActiveModule(selectedModule);
     }
 
     return (
-        <View style={styles.container}>
-            <Header title="Shopping List" />
-            <AddItem addItem={ addItem } />
-            <FlatList
-                data={ items }
-                renderItem={ ({item}) => (
-                    <ListItem item={ item } deleteItem={ deleteItem } />
-                )}/>
+        <View style={ styles.container }>
+            <Header
+                activeModule={ activeModule }
+                selectModule={ selectModule }
+                menuOpen={ menuOpen }
+                toggleMenu={ toggleMenu }
+                subModules={ subModules } />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
 });
 
 export default App;
--- mobile/ShoppingList/components/Header.js
+++ mobile/ShoppingList/components/Header.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, ListItem, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+import { getPercent } from '../utils/math.js';
 
-const Header = ({ title }) => {
+const Header = ({ activeModule, selectModule, menuOpen, toggleMenu, subModules }) => {
     return (
-        <View style={ styles.header }>
-            <Text style={ styles.text }>{ title }</Text>
+        <View style={ styles.column }>
+            <View style={ styles.row }>
+                <Icon style={ styles.menuIcon } name={ menuOpen ? "chevron-up" : "chevron-down" } size={ getPercent(8) } color="#282828" onPress={ toggleMenu } />
+                <View style={ styles.menuTitleWrapper }>
+                    <Text style={ styles.menuTitle }>{ activeModule ? activeModule : "Select app" }</Text>
+                </View>
+            </View>
+            <View style={ menuOpen ? styles.subModuleMenu : styles.displayNone }>
+                <FlatList
+                    data={ subModules }
+                    renderItem={ ({ item }) => (
+                        <TouchableOpacity style={ styles.subModuleMenuRow }>
+                            <View key={ item.id } selectModule={ item.name }>
+                                <Text style={ styles.subModuleMenuRowTitle }>{ item.name }</Text>
+                            </View>
+                        </TouchableOpacity>
+                    )}/>
+            </View>
         </View>
     )
 }
@@ -14,15 +32,56 @@ Header.defaultProps = {
 }
 
 const styles = StyleSheet.create({
-    header: {
-        height: 60,
-        padding: 15,
-        backgroundColor: 'darkslateblue'
+    displayNone: {
+        display: "none"
     },
-    text: {
-        color: 'white',
-        fontSize: 23,
-        textAlign: 'center'
+    column: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        justifyContent: "flex-start"
+    },
+    row: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "stretch",
+        justifyContent: "center",
+        borderBottomWidth: 1,
+        borderBottomColor: "black"
+    },
+    menuIcon: {
+        flex: 0,
+        padding: 2
+    },
+    menuTitleWrapper: {
+        flex: 1,
+        display: "flex",
+        alignItems: "flex-start",
+        justifyContent: "center",
+        paddingLeft: getPercent(1.5),
+        paddingRight: getPercent(1.5),
+    },
+    menuTitle: {
+        fontSize: 24
+    },
+    subModuleMenu: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        justifyContent: "flex-start"
+    },
+    subModuleMenuRow: {
+        display: "flex",
+        flex: 1,
+        paddingLeft: 10,
+        paddingTop: 5,
+        paddingBottom: 5,
+        width: getPercent(100),
+        borderBottomWidth: 1,
+        borderBottomColor: '#808080'
+    },
+    subModuleMenuRowTitle: {
+        fontSize: 20
     }
 });
 
