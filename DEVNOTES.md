92c06c3ef3d6d7e8d2ea21be3c56c9b2d8d502fa
Jacob Cunningham <jcun4128@gmail.com>
2023-06-15 18:39:49 -0500

    integrate drawing module

--- DEVNOTES.md
+++ DEVNOTES.md
@@ -1,6 +1,34 @@
 Desires
 
-- [ ] drawing app
+- [x] drawing app (06/15/2023)
     - got this great [starter code](https://stackoverflow.com/questions/2368784/draw-on-html5-canvas-using-a-mouse) here, figured out how to make it with my drawing tablet, this will be embedded for basic drawings
     - it'll be interesting figuring out how to save data
     - possible to save to image, looking into keeping drawing
+
+06/15/2023
+
+Desires lol
+
+Yeah I'm going to add the drawing app that I made it's over here separate repo
+
+https://github.com/jdc-cunningham/drawing-notes-app
+
+It's a piece of crap but works, has pressure support
+
+6:22 PM
+
+dang it... dealing with node-sass on win 10 right now, this branch need to remove
+
+also thinking about app vs. web... specifically full screen for when in tablet mode/folded over
+
+6:29 PM
+
+oh no this one has an offset problem
+
+well now is the time...
+
+oh dang, when you load an image, have to clear existing canvas first
+
+6:39 PM
+
+nice fixed the offset
--- desktop/reactjs/src/modules/drawing/CanvasDrawingModule.js
+++ desktop/reactjs/src/modules/drawing/CanvasDrawingModule.js
@@ -1,10 +1,229 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import './CanvasDrawingModule.scss';
+import Pressure from 'pressure';
+import RedX from './assets/icons/uxwing_close-icon.svg';
+import DrawingMenu from './components/drawing-menu/DrawingMenu';
 
 const CanvasDrawingModule = (props) => {
-    return <div className="cpa__module" id="module--canvas-drawing">
-        <h2>Canvas Drawing Module</h2>
-    </div>
+	const [menuOpen, setMenuOpen] = useState(false);
+	const [activeDrawing, setActiveDrawing] = useState({
+		name: 'Drawing title',
+		id: 0,
+		topics: '',
+	});
+	const [color, setColor] = useState('black');
+	const [colorsVisible, setColorsVisible] = useState(false);
+	const [drawStarted, setDrawStarted] = useState(false); // specifically for colors
+	const [savingState, setSavingState] = useState(''); // saving, saved
+
+	// variant from basic demo here:
+	// https://stackoverflow.com/questions/2368784/draw-on-html5-canvas-using-a-mouse
+	let canvas = false;
+	var ctx = false;
+	let flag = false;
+	let prevX = 0;
+	let currX = 0;
+	let prevY = 0;
+	let currY = 0;
+	let dot_flag = false;
+	var y = 2;
+
+	const colors = [
+		'black',
+		'white',
+		'red',
+		'blue',
+		'green',
+		'gray'
+	];
+
+	const toggleColors = () => {
+		setColorsVisible(!colorsVisible);
+	}
+
+	const initPressure = () => {
+		const pressures = [];
+
+		// reducer
+		// https://stackoverflow.com/a/41452260
+		const average = array => array.reduce((a, b) => a + b) / array.length;
+
+		Pressure.set('#canvas', {
+			start: function(event){
+				// this is called on force start
+				if (!drawStarted) {
+					setDrawStarted(true);
+				}
+			},
+			end: function(){
+				// this is called on force end
+			},
+			startDeepPress: function(event){
+				// this is called on "force click" / "deep press", aka once the force is greater than 0.5
+			},
+			endDeepPress: function(){
+				// this is called when the "force click" / "deep press" end
+			},
+			change: function(force, event){
+				// this is called every time there is a change in pressure
+				// force will always be a value from 0 to 1 on mobile and desktop
+
+				if (pressures.length === 5) {
+					pressures.shift();
+				}
+
+				pressures.push(Math.floor(force * 5));
+
+				y = average(pressures);
+			},
+			unsupported: function(){
+				// NOTE: this is only called if the polyfill option is disabled!
+				// this is called once there is a touch on the element and the device or browser does not support Force or 3D touch
+			}
+		});
+	}
+
+	const getCtx = () => {
+		return document.getElementById('canvas').getContext("2d");
+	}
+
+	const setCanvasSize = () => {
+		const container = document.querySelector('.canvas-drawing-module');
+		const header = document.querySelector('.canvas-drawing-module__header');
+
+		canvas = document.getElementById('canvas');
+		canvas.width = container.clientWidth - 10; // scrollbar
+		canvas.height = container.clientHeight - header.offsetHeight - 10;
+	}
+
+	const init = () => {
+		const container = document.querySelector('.canvas-drawing-module');
+		const header = document.querySelector('.canvas-drawing-module__header');
+
+		canvas = document.getElementById('canvas');
+		canvas.width = container.clientWidth - 10; // scrollbar
+		canvas.height = container.clientHeight - header.offsetHeight - 10;
+		ctx = canvas.getContext("2d");
+
+		canvas.addEventListener("mousemove", function (e) {
+			findxy('move', e)
+		}, false);
+
+		canvas.addEventListener("mousedown", function (e) {
+			findxy('down', e)
+		}, false);
+
+		canvas.addEventListener("mouseup", function (e) {
+			findxy('up', e)
+		}, false);
+
+		canvas.addEventListener("mouseout", function (e) {
+			findxy('out', e)
+		}, false);
+
+		initPressure();
+	}
+
+	const draw = () => {
+		ctx.beginPath();
+		ctx.moveTo(prevX, prevY);
+		ctx.lineTo(currX, currY);
+		ctx.lineWidth = y;
+		ctx.stroke();
+		ctx.closePath();
+	}
+
+	const erase = () => {
+		var m = window.confirm("Want to clear");
+		if (m) {
+			// still referring to init 0 values
+			let canvas = document.getElementById('canvas');
+			getCtx().clearRect(0, 0, canvas.width, canvas.height);
+		}
+	}
+
+	const findxy = (res, e) => {
+		if (res === 'down') {
+			prevX = currX;
+			prevY = currY;
+			currX = e.clientX - canvas.offsetLeft - 90;
+			currY = e.clientY - canvas.offsetTop - 10;
+
+			flag = true;
+			dot_flag = true;
+
+			if (dot_flag) {
+				ctx.beginPath();
+				ctx.fillRect(currX, currY, 2, 2);
+				ctx.closePath();
+				dot_flag = false;
+			}
+		}
+
+		if (res === 'up' || res === "out") {
+			flag = false;
+		}
+
+		if (res === 'move') {
+			if (flag) {
+				prevX = currX;
+				prevY = currY;
+				currX = e.clientX - canvas.offsetLeft - 90;
+				currY = e.clientY - canvas.offsetTop - 10;
+				draw();
+			}
+		}
+	}
+
+	const toggleMenu = () => {
+		setMenuOpen(!menuOpen);
+	}
+
+	const getCanvas = () => document.getElementById('canvas');
+
+	useEffect(() => {
+		if (drawStarted) {
+			getCtx().strokeStyle = color;
+			toggleColors();
+		}
+	}, [color])
+
+	useEffect(() => {
+		init();
+
+		window.addEventListener('resize', function(event) {
+			setCanvasSize();
+		}, true);
+	}, [])
+
+	return (
+		<div className="canvas-drawing-module" id="module--canvas-drawing">
+			<div className="canvas-drawing-module__header">
+				<button className="canvas-drawing-module__header-toggle-menu-btn" type="button" onClick={() => toggleMenu()}>Menu</button>
+				<h2>{activeDrawing.name}</h2>
+				<DrawingMenu
+					canvas={getCanvas()}
+					menuOpen={menuOpen}
+					setMenuOpen={setMenuOpen}
+					activeDrawing={activeDrawing}
+					setActiveDrawing={setActiveDrawing}
+					setSavingState={setSavingState}
+					erase={erase}
+				/>
+			</div>
+			<canvas id="canvas"/>
+			<button className="canvas-drawing-module__clear-btn" type="button" title="clear drawing" onClick={() => erase()}>
+				<img className="canvas-drawing-module__clear-btn-img" src={RedX} alt="clear drawing"/>
+			</button>
+			<div className={`canvas-drawing-module__active-color ${color} ${colorsVisible ? '' : 'open'}`} onClick={() => toggleColors()} title="click to pick color"></div>
+			<div className={`canvas-drawing-module__colors ${colorsVisible ? 'open' : ''}`}>
+				<div className="canvas-drawing-module__colors-container">
+					{colors.map((color, index) => <div key={index} className={`canvas-drawing-module__color ${color}`} onClick={() => setColor(color)} alt={`${color}`} title={`use ${color}`}></div>)}
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default CanvasDrawingModule;
